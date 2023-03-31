876710eac494c239b7f3f3b85fb271917640c5fc
Vladyslav-H <97888612+Vladyslav-H@users.noreply.github.com>
2023-03-31 16:52:00 +0300

    Update FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,4 +1,4 @@
-import { Component, useState } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import {
@@ -6,39 +6,7 @@ import {
   Button,
 } from 'components/FeedbackOptions/FeedbackOptions.styled';
 
-// export class FeedbackOptionsClass extends Component {
-//   static defaultProps = {
-//     options: [],
-//   };
-
-//   static propTypes = {
-//     options: PropTypes.array.isRequired,
-//     onLeaveFeedback: PropTypes.func.isRequired,
-//   };
-
-//   render() {
-
-//     return (
-//       <FeedbackWrapp>
-//         {this.props.options.map(btn => (
-//           <Button
-//             key={btn}
-//             value={btn}
-//             variant={btn}
-//             type="button"
-//             onClick={btn => this.props.onLeaveFeedback(btn)}
-//           >
-//             {btn}
-//           </Button>
-//         ))}
-//       </FeedbackWrapp>
-//     );
-//   }
-// }
-
-export const FeedbackOptions = ({options, onLeaveFeedback}) => {
-  // const [options, setOptions] = useState([]);
-
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <FeedbackWrapp>
       {options.map(btn => (
